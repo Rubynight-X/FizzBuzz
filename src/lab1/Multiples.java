52dfc8d13c1a77f9fc136257505e75884b7ff692
Claire <claire77.xiang@gmail.com>
2026-09-16 02:50:46 -0400

    add Multiples.java

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,26 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args){
+
+        int multiplesOf3 = 0;
+        int multiplesOf5 = 0;
+        int multiplesOf3and5 = 0;
+
+        for (int i = 1; i < 1000; i++) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            if (divisibleBy3 && divisibleBy5) {
+                multiplesOf3and5 += 1;
+            } else if (divisibleBy3) {
+                multiplesOf3 += 1;
+            } else if (divisibleBy5) {
+                multiplesOf5 += 1;
+            }
+        }
+
+        int multiplesOf3or5 = multiplesOf3 + multiplesOf5 + multiplesOf3and5;
+        System.out.println(multiplesOf3or5);
+    }
+}
